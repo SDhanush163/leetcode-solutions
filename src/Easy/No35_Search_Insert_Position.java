3f9e8713d5ecdc98f73009cd1647001dba7bf12f
Dhanush <sdhanush163@gmail.com>
2021-01-13 18:12:19 +0530

    Added #35 Search Insert Position

---
+++ src/Easy/No35_Search_Insert_Position.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class No35_Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target)
+                return i;
+        }
+        return nums.length;
+    }
+}
