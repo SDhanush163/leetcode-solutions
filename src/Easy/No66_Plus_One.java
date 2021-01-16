047a2482d3e73f95d10c74419d1e879470e5b137
Dhanush <sdhanush163@gmail.com>
2021-01-16 16:03:42 +0530

    Added #66 Plus one

---
+++ src/Easy/No66_Plus_One.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class No66_Plus_One {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1;i >= 0; i--) {
+            if (digits[i] == 9)
+                digits[i] = 0;
+            else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
